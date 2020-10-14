0e369fae984c28421580630e54bbbce312c39e45
Gustavo D. Dainezi <gustavo.dainezi@fatec.sp.gov.br>
2020-10-14 13:48:26 -0300

    Estilização do mapa de orfanatos

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -5,6 +5,20 @@ const map = L.map('mapid').setView([-23.5423, -46.369], 16);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
 // Criar icon
+const icon = L.icon({
+    iconUrl: './public/images/map-marker.svg',
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popUpAnchor: [170, 2]
+})
 
-//
-L.marker([-23.5423, -46.369]).addTo(map).bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
+// Criar popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minwidth: 240,
+    minheight: 240
+}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"</a>')
+
+// Criar e adicionar marcador
+L.marker([-23.5423, -46.369], {icon: icon}).addTo(map).bindPopup(popup)
