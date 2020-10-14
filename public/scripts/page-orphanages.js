488dd67c412a2cb82466a7efdc9b2cc68453fa48
Gustavo D. Dainezi <gustavo.dainezi@fatec.sp.gov.br>
2020-10-14 14:43:05 -0300

    Atualização do mapa

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,5 +1,5 @@
 // Criar mapa
-const map = L.map('mapid').setView([-23.5423, -46.369], 16);
+const map = L.map('mapid').setView([-23.5423, -46.369], 15);
 
 // Criar e adicionar TileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
@@ -9,7 +9,7 @@ const icon = L.icon({
     iconUrl: './public/images/map-marker.svg',
     iconSize: [58, 68],
     iconAnchor: [29, 68],
-    popUpAnchor: [170, 2]
+    popupAnchor: [160, 1]
 })
 
 // Criar popup overlay
@@ -18,7 +18,7 @@ const popup = L.popup({
     className: 'map-popup',
     minwidth: 240,
     minheight: 240
-}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"</a>')
+}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>')
 
 // Criar e adicionar marcador
-L.marker([-23.5423, -46.369], {icon: icon}).addTo(map).bindPopup(popup)
+L.marker([-23.5423, -46.369], { icon }).addTo(map).bindPopup(popup)
