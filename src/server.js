8ea31caf9b16230cd8ff44d6d43082bd01e52dd2
Gustavo D. Dainezi <gustavo.dainezi@fatec.sp.gov.br>
2020-10-16 12:40:46 -0300

    Primeiros testes no servidor

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const path = require('path')
+
 const server = express()
+server.use(express.static('public'))
 
-server.get('/', () => {
-
+server.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 server.listen(5500)
