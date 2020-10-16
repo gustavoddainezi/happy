0af3f083d5ebb56e1680fae6a25f1ab5411825ed
Gustavo D. Dainezi <gustavo.dainezi@fatec.sp.gov.br>
2020-10-16 13:52:37 -0300

    Criação do diretório database

---
+++ src/database/fakedata.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-23.5423", 
+        lng: "-46.369",
+        name: "Lar Betânia",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidades social.",
+        images: ["https://images.unsplash.com/photo-1586978450997-3251b1d493e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+                 "https://images.unsplash.com/photo-1599003037886-f8b50bc290c8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"],
+        instructions: "Venha se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de Visitas Das 18h até 8h",
+        opening_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-23.5423", 
+        lng: "-46.339",
+        name: "Lar das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidades social.",
+        images: ["https://images.unsplash.com/photo-1586978450997-3251b1d493e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+                 "https://images.unsplash.com/photo-1599003037886-f8b50bc290c8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"],
+        instructions: "Venha se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de Visitas Das 8h até as 18h",
+        opening_on_weekends: "0"
+    }
+]
