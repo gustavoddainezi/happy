9aee6a5aee7473b31f65b12d20ab58a9f3d81a34
Gustavo D. Dainezi <gustavo.dainezi@fatec.sp.gov.br>
2020-10-17 21:10:12 -0300

    Modificação do servidor ao salvar um novo orfanato

--- src/server.js
+++ src/server.js
@@ -4,15 +4,17 @@ const pages = require('./pages.js');
 
 const server = express()
 server
-.use(express.static('public'))
-.set('views', path.join(__dirname, "views"))
-.set('view engine', 'hbs')
+    .use(express.urlencoded({ extended: true }))
+    .use(express.static('public'))
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'hbs')
 
 server
     .get('/', pages.index)
     .get('/orphanage', pages.orphanage)
     .get('/orphanages', pages.orphanages)
     .get('/create-orphanage', pages.createOrphanage)
+    .post('/save-orphanage', pages.saveOrphanage)
 
 
 server.listen(5500)
